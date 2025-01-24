ef0f9c4dfeddfcdc0468feeb4c07bfe6b1df7163
apnahindustan <apnahindustanpvt@gmail.com>
2025-01-24 19:39:26 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+if (localStorage.getItem("fire_local")) {
+  window.location.href = "index.html";
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  var url = "https://script.google.com/macros/s/AKfycbyHRssgh7PrRWFB7I-nK2ml6wL3Urv6VepJRsUmTTn5PbUk5coStGlMlHMijuQPEMc_Lg/exec";
+
+  var data = {
+    "username": username,
+    "password": password
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(data)
+  })
+  .then(response => response.text())
+  .then(responseText => {
+    if (responseText === "success") {
+      localStorage.setItem("fire_local", username);
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid username or password!");
+    }
+  })
+  .catch(error => console.error('Error:', error));
+});
